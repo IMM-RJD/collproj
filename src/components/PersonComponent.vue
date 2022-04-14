cb23dacdf6e2ccc4010699777ef34dda28d9538b
IMM-RJD <real_jane_doe@gmx.de>
2022-04-14 14:55:25 +0200

    update personCP stretch items, expand description

--- src/components/PersonComponent.vue
+++ src/components/PersonComponent.vue
@@ -1,56 +1,57 @@
 <template>
   <div
-    class="q-pa-md fit row wrap justify-evenly items-start content-start q-gutter-lg"
+    class="q-pa-md fit row wrap justify-evenly items-stretch content-start q-gutter-lg"
   >
-    <div v-for="person in persons" :key="person.id">
-      <q-card class="bg-secondary text-white imm-person-card">
-        <q-img
-          width="100%"
-          height="250px"
-          class=""
-          fit="fill"
-          :src="person.imgSrc"
-        ></q-img>
-        <div class="q-pb-none">
-          <q-card-section class="q-pb-xs">
-            <div class="text-h6">
-              {{ person.firstName }} {{ person.lastName }}
-            </div>
-            <div class="text-subtitle2">{{ person.role }}</div>
-          </q-card-section>
+    <q-card
+      v-for="person in persons"
+      :key="person.id"
+      class="bg-secondary text-white imm-person-card"
+    >
+      <q-img
+        width="100%"
+        height="250px"
+        class="imm-person-card-img"
+        fit="fill"
+        :src="person.imgSrc"
+      ></q-img>
+      <div class="q-pb-none imm-person-card-content-wrapper">
+        <q-card-section class="q-pb-xs">
+          <div class="text-h6">
+            {{ person.firstName }} {{ person.lastName }}
+          </div>
+          <div class="text-subtitle2">{{ person.role }}</div>
+        </q-card-section>
 
-          <q-card-section>
-            {{ person.description }}
-          </q-card-section>
+        <q-card-section class="imm-person-card-content-description">
+          {{ person.description }}
+        </q-card-section>
 
-          <q-separator dark />
-
-          <q-card-actions vertical class="q-pb-none q-pt-md">
-            <q-btn
-              unelevated
-              class="q-mb-sm"
-              :no-caps="true"
-              align="left"
-              type="a"
-              :href="'tel:' + person.phone"
-              icon="fa-solid fa-phone"
-              :label="person.phone"
-            ></q-btn>
-            <q-btn
-              flat
-              unelevated
-              class="q-mb-sm imm-ml-0-important"
-              :no-caps="true"
-              align="left"
-              type="a"
-              :href="'mailto:' + person.email"
-              icon="fa-solid fa-envelope"
-              :label="person.email"
-            ></q-btn>
-          </q-card-actions>
-        </div>
-      </q-card>
-    </div>
+        <q-separator dark />
+        <q-card-actions vertical class="q-pb-none q-pt-md">
+          <q-btn
+            unelevated
+            class="q-mb-sm"
+            :no-caps="true"
+            align="left"
+            type="a"
+            :href="'tel:' + person.phone"
+            icon="fa-solid fa-phone"
+            :label="person.phone"
+          ></q-btn>
+          <q-btn
+            flat
+            unelevated
+            class="q-mb-sm imm-ml-0-important"
+            :no-caps="true"
+            align="left"
+            type="a"
+            :href="'mailto:' + person.email"
+            icon="fa-solid fa-envelope"
+            :label="person.email"
+          ></q-btn>
+        </q-card-actions>
+      </div>
+    </q-card>
   </div>
 </template>
 
