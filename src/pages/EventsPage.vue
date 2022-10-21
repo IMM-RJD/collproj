f8a23b4040740c84559f70581bfd6dcfb6420718
IMM-RJD <real_jane_doe@gmx.de>
2022-10-21 11:41:05 +0200

    update event buttons

--- src/pages/EventsPage.vue
+++ src/pages/EventsPage.vue
@@ -2,8 +2,9 @@
   <q-page padding class="q-mx-md">
     <!-- content -->
     <h4 class="q-my-lg">Bevorstehende Events</h4>
-    <div style="font-size: 24px; margin-bottom: 10px">
-      Schattenwelten und Lichtgestalten
+    <div style="font-size: 24px">Schattenwelten und Lichtgestalten</div>
+    <div style="font-size: 20px; margin-bottom: 15px">
+      als Teil des Programms der Trierer Unterwelten
     </div>
     <q-img
       height="300px"
@@ -13,18 +14,39 @@
 
     <div style="font-size: 18px; margin-top: 10px; margin-bottom: 10px">
       <q-btn
-        label="Visuelles anzeigen"
+        label="Tickets"
         color="secondary"
-        @click="flyer = true"
-        style="margin-top: 10px; margin-bottom: 10px; margin-right: 20px"
-      />
-      <q-btn
-        label="zu den Tickets"
-        color="primary"
         href="https://www.ticket-regional.de/kollektivmehrklang"
         target="_blank"
-        style="float: right; margin-top: 10px; margin-bottom: 10px"
+      /><br />
+      <q-btn
+        label="Lineup"
+        class="q-my-sm km-prim-sec-gradient"
+        @click="flyerLineup = true"
       />
+      <q-dialog
+        v-model="flyerLineup"
+        transition-show="rotate"
+        transition-hide="rotate"
+        :square="true"
+      >
+        <q-img
+          fit="fill"
+          loading="lazy"
+          src="src/assets/projects/uw/dj_liste.jpg"
+          width="400px"
+          height="400px"
+        ></q-img>
+      </q-dialog>
+      <br />
+      <div style="display: flex; flex-direction: row-reverse">
+        <q-btn
+          label="Visuelles"
+          color="primary"
+          class="float-right"
+          @click="flyer = true"
+        />
+      </div>
     </div>
     <q-dialog v-model="flyer" transition-show="rotate" transition-hide="rotate">
       <q-carousel
@@ -47,52 +69,45 @@
           <q-img
             width="380px"
             loading="lazy"
-            src="src/assets/projects/uw/anzeige.jpg"
+            src="src/assets/projects/uw/anzeige_ohneBild.jpg"
           ></q-img>
         </q-carousel-slide>
         <q-carousel-slide :name="3" class="imm-p-0-important">
           <q-img
             width="580px"
             loading="lazy"
-            src="src/assets/projects/uw/dj_liste.jpg"
-          ></q-img>
-        </q-carousel-slide>
-        <q-carousel-slide :name="4" class="imm-p-0-important">
-          <q-img
-            width="580px"
-            loading="lazy"
             src="src/assets/projects/uw/ll_mood.jpg"
           ></q-img>
         </q-carousel-slide>
-        <q-carousel-slide :name="5" class="imm-p-0-important">
+        <q-carousel-slide :name="4" class="imm-p-0-important">
           <q-img
             width="400px"
             loading="lazy"
             src="src/assets/projects/uw/innen.jpg"
           ></q-img>
         </q-carousel-slide>
-        <q-carousel-slide :name="6" class="imm-p-0-important">
+        <q-carousel-slide :name="5" class="imm-p-0-important">
           <q-img
             width="400px"
             loading="lazy"
             src="src/assets/projects/uw/aeste.jpg"
           ></q-img>
         </q-carousel-slide>
-        <q-carousel-slide :name="7" class="imm-p-0-important">
+        <q-carousel-slide :name="6" class="imm-p-0-important">
           <q-img
             width="580px"
             loading="lazy"
             src="src/assets/projects/uw/rotweis1.jpg"
           ></q-img>
         </q-carousel-slide>
-        <q-carousel-slide :name="8" class="imm-p-0-important">
+        <q-carousel-slide :name="7" class="imm-p-0-important">
           <q-img
             width="580px"
             loading="lazy"
             src="src/assets/projects/uw/rotweis2.jpg"
           ></q-img>
         </q-carousel-slide>
-        <q-carousel-slide :name="9" class="imm-p-0-important">
+        <q-carousel-slide :name="8" class="imm-p-0-important">
           <q-img
             width="580px"
             loading="lazy"
@@ -101,7 +116,7 @@
         </q-carousel-slide>
       </q-carousel>
     </q-dialog>
-    <div style="font-size: 18px">
+    <div style="font-size: 18px; margin-top: 20px">
       <a
         href="https://www.trierer-unterwelten.de/"
         target="blank"
@@ -110,8 +125,10 @@
         Unterwelten-Festival
       </a>
       vom 28.10. bis 19.11.2022 mit dem Projekt "Schattenwelten und
-      Lichtgestalten", je 3 Veranstaltungen pro Woche im Frankenturm Trier -
-      Tanztheaterstück plus musikalisches Happening.
+      Lichtgestalten". <br />
+      Insgesamt 9 Veranstaltungen plus die "Lange Nacht der Unterwelten" (Route
+      2) am 12.11. im Frankenturm Trier - Tanztheaterstück mit
+      musikalisch-elektronischem Sound im Anschluss.
     </div>
     <q-separator class="q-my-md" />
     <div style="font-size: 18px">
@@ -123,8 +140,10 @@
         Unterwelten-Festival
       </a>
       from 28.10. to 19.11.2022 with the project "Schattenwelten und
-      Lichtgestalten" ("Shadow worlds and light figures"), 3 Events per week in
-      the Frankenturm Trier - dance theater play plus musical happening.
+      Lichtgestalten" ("Shadow worlds and light figures"). <br />
+      A total of 9 events plus the "Lange Nacht der Unterwelten" ("Long Night of
+      the Underworlds") (Route 2) on November 12th. in the Frankenturm Trier -
+      dance theatre play with musical-electronic sound afterwards.
     </div>
     <q-img
       class="q-my-md"
@@ -151,6 +170,7 @@ export default defineComponent({
   setup() {
     return {
       flyer: ref(false),
+      flyerLineup: ref(false),
       slide: ref(1),
       fullscreen: ref(false),
       pastEvents: ref(false),
